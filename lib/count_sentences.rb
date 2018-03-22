0b087a1eb6c3b9449e03638a5bb43b236845ad67
Karen G <kgraves0701@gmail.com>
2018-03-22 22:41:37 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     counter = 0
-    array = self.split("."||"!"||"?"|"  ")
+    array = self.split("."||"!"||"?")
     puts array
     array.each do |string|
       if string != ""
